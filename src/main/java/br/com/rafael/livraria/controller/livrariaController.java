e251fa3c68b0b6a6b6af7b63529881aecfc90db8
Rafael Carvalho <rafaelcarvalhocs2016@gmail.com>
2020-01-29 12:07:54 -0300

    Adicionar botam que leva para livros cadastrados

--- src/main/java/br/com/rafael/livraria/controller/livrariaController.java
+++ src/main/java/br/com/rafael/livraria/controller/livrariaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class livrariaController {
@@ -21,6 +22,17 @@ public class livrariaController {
     @PostMapping("/cadastrar")
     public String cadastroLivro(livrariaModel livro){
         service.cadastrarLivro(livro); // aqui ele enviara os dados para o metodo cadastrarLivro e os dados serao salvos em livro
-        return "index";
+        return "index"; // o return sempre vai retornar a pagina que voce criou
     }
+
+
+
+   @GetMapping("/livros")
+   public ModelAndView listarLivro(){
+    ModelAndView paginaLivro = new ModelAndView("listarLivro");
+  Iterable<livrariaModel>  livro = service.listarLivros();
+  paginaLivro.addObject("livro", livro);
+    return paginaLivro;
+    }
+
 }
--- src/main/java/br/com/rafael/livraria/service/livrariaService.java
+++ src/main/java/br/com/rafael/livraria/service/livrariaService.java
@@ -7,10 +7,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class livrariaService {
-    @Autowired // indeção de dependencia, ou seja, essa classe está dependente de outra
+    @Autowired // injeção de dependencia, ou seja, essa classe está dependente de outra
     private livrariaRepository repository; // essa classe esta dependete da classe repository
 
     public livrariaModel cadastrarLivro(livrariaModel livro) {
         return repository.save(livro); //aqui os dados serao salvos no livro
     }
+
+    public Iterable<livrariaModel> listarLivros(){ //Aqui estamos criando um metodo, Iterable vai criar uma lista,
+    Iterable<livrariaModel>  livro = repository.findAll(); // aqui chamamos o repositorio e usamos o find.all, ele é do tipo Iterable, e vai puxar tudo que foi cadastrado no formato de lista
+    return livro; // variavel livro que esta a cima
+    }
+
 }
